6328879e699b29ec21ffbd0ccabb07feeebfa76f
rishikamurugesh602 <rishikamurugesh@gmail.com>
2026-07-12 19:29:21 +0530

    Update productexceptself.py

--- productexceptself.py
+++ productexceptself.py
@@ -1,1 +1,22 @@
 
+n = int(input())
+arr = list(map(int, input().split()))
+
+def solution():
+    prefix = [1] * n
+    suffix = [1] * n
+    ans = [1] * n
+
+    for i in range(1, n):
+        prefix[i] = prefix[i - 1] * arr[i - 1]
+
+    for i in range(n - 2, -1, -1):
+        suffix[i] = suffix[i + 1] * arr[i + 1]
+
+    for i in range(n):
+        ans[i] = prefix[i] * suffix[i]
+
+    return ans
+
+ans = solution()
+print(*ans)
