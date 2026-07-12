ddd599b66d29eaa7e51aeb53b3869c6c5d94675c
rishikamurugesh602 <rishikamurugesh@gmail.com>
2026-07-12 10:58:25 +0530

    Create containerwater.py

---
+++ containerwater.py
@@ -0,0 +1,22 @@
+n=int(input())
+height=list(map(int,input().split()))
+def solution():
+    i=0
+    j=n-1
+    max_area=0
+    while i<j:
+        area=(j-i)*min(height[i],height[j])
+        max_area=max(max_area,area)
+        if height[i]<=height[j]:
+            i+=1
+        else:
+            j-=1
+    return max_area
+        
+anss=solution()
+print(anss)
+        
+
+    
+        
+            
