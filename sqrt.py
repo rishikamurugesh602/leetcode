488b3a65f72c78a08b75228121277f7183cb9639
rishikamurugesh602 <rishikamurugesh@gmail.com>
2026-07-15 19:19:50 +0530

    Create sqrt.py

---
+++ sqrt.py
@@ -0,0 +1,17 @@
+def mySqrt(x):
+    low = 0
+    high = x
+    ans = 0
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if mid * mid == x:
+            return mid
+        elif mid * mid < x:
+            ans = mid
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return ans
